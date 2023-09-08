4cf67560d438435b924f33fa4c1dcdd81ae16fcc
Saurabh Painuly <123741420+saurabhash15@users.noreply.github.com>
2023-09-08 20:51:06 +0530

    Updates Create.sql

---
+++ Programs/program1/create.sql
@@ -0,0 +1,8 @@
+/*1.Create a new table “Employee” with attribute EMPCODE,NAME(6),SALARY.*/
+
+CREATE TABLE EMPLOYEE
+(EMPCODE NUMBER,
+NAME VARCHAR2(6),
+SALARY NUMBER);
+
+DESC EMPLOYEE; /*displaying structure*/
